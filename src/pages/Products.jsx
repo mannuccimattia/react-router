0a632df2d087c943f866f0249a9546e2ad5425e8
mannuccimattia <mannuccimattia@gmail.com>
2025-05-15 16:14:21 +0200

    [UPD] Done products list style

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -22,20 +22,40 @@ const Products = () => {
   return (
     <>
       <HeadNavbar />
-      <div>
-        <h1>Products</h1>
+      <div className="container">
+
+        <div className="row">
+          <div className="col-12">
+            <h1>Products</h1>
+          </div>
+        </div>
+
+        <div className="row g-3">
+          {products === null ? <div>Loading...</div>
+            :
+            <div className="row g-3">
+              {products.map(product => (
+                <div
+                  className="col-12 col-md-6 col-lg-4"
+                  key={product.id}
+                >
+                  <div className="card h-100 d-flex flex-column">
+                    <div className="card-image-top">
+                      <img src={product.image} className='img-fluid' alt={product.description} />
+                    </div>
+                    <div className="card-body p-0">
+                      <h5 className="bg-primary p-3 fw-semibold">
+                        {product.title}
+                      </h5>
+                      <p className="my-2 text-center price">Price: € {product.price.toFixed(2, 0)}</p>
+                      <p className="desc">{product.description}</p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>}
+        </div>
       </div>
-      <ul>
-        {products === null ? <div>Loading...</div>
-          :
-          <div>
-            {products.map(prod => (
-              <li key={prod.id}>
-                {prod.title}
-              </li>
-            ))}
-          </div>}
-      </ul>
     </>
   )
 }
