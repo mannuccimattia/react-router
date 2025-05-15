48a3bbb3b06b4620d5f430df6f09083065cb02cf
mannuccimattia <mannuccimattia@gmail.com>
2025-05-15 15:18:44 +0200

    [UPD] Done products list - raw, unstyled

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,13 +1,41 @@
+import { useState, useEffect } from "react"
 import { NavLink } from "react-router-dom"
+import axios from "axios"
+
 import HeadNavbar from "../components/HeadNavbar"
 
 const Products = () => {
+
+  const [products, setProducts] = useState(null);
+
+  const getProducts = () => {
+    axios.get('https://fakestoreapi.com/products')
+      .then(res => {
+        setProducts(res.data);
+      });
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <>
       <HeadNavbar />
       <div>
         <h1>Products</h1>
       </div>
+      <ul>
+        {products === null ? <div>Loading...</div>
+          :
+          <div>
+            {products.map(prod => (
+              <li key={prod.id}>
+                {prod.title}
+              </li>
+            ))}
+          </div>}
+      </ul>
     </>
   )
 }
