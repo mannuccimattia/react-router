9beb8ea0d4ac516cc17345c9eec2981e00ecc627
mannuccimattia <mannuccimattia@gmail.com>
2025-05-16 15:17:40 +0200

    [UPD] Done styling

--- src/pages/products/ProductDetail.jsx
+++ src/pages/products/ProductDetail.jsx
@@ -28,7 +28,7 @@ const ProductDetail = () => {
             <h1 className=' text-center'>Details</h1>
             <hr />
 
-            <div className="row g-4 mt-3">
+            <div className="row g-4 my-3">
               {product === null ?
                 (
                   <div className="col-12 d-flex justify-content-center">
@@ -36,42 +36,51 @@ const ProductDetail = () => {
                   </div>
                 ) :
                 (
-                  <div className="col-12">
-                    <div className="image-container">
-                      <img src={product.image} alt="" className='img-fluid' />
+                  <>
+                    <div className="col-12 col-md-6">
+                      <div className="image-container">
+                        <img src={product.image} alt="" className='img-fluid' />
+                      </div>
                     </div>
-                    <h1>{product.title}</h1>
-                    <div className='my-3 text-center'>
-                      <em>{product.price} €</em>
+
+                    <div className="col-12 col-md-6">
+                      <h3>{product.title}</h3>
+                      <div className='my-3'>
+                        <strong className='text-success'>{product.price} €</strong>
+                      </div>
+                      <div className='my-3'>
+                        <em>{product.category}</em>
+                      </div>
+                      <p>{product.description}</p>
+
+                      <div className="row my-5 justify-content-start">
+                        <div className="col-auto">
+                          <button
+                            className='btn btn-primary'
+                            onClick={() => {
+                              navigate(`/products/${parseInt(id) - 1}`)
+                            }}
+                            disabled={id == 1 ? true : false}
+                          >
+                            Prev
+                          </button>
+                        </div>
+                        <div className="col-auto">
+                          <button
+                            className='btn btn-primary'
+                            onClick={() => {
+                              navigate(`/products/${parseInt(id) + 1}`)
+                            }}
+                          >
+                            Next
+                          </button>
+                        </div>
+                      </div>
                     </div>
-                    <div className='my-3 text-center'>
-                      <strong>{product.category}</strong>
-                    </div>
-                    <p>{product.description}</p>
-                  </div>
+                  </>
+
                 )
               }
-            </div>
-
-            <div className="row my-3 justify-content-around">
-              <button
-                className='btn btn-primary'
-                onClick={() => {
-                  navigate(`/products/${parseInt(id) - 1}`)
-                }}
-                disabled={id == 1 ? true : false}
-              >
-                Prev
-              </button>
-
-              <button
-                className='btn btn-primary'
-                onClick={() => {
-                  navigate(`/products/${parseInt(id) + 1}`)
-                }}
-              >
-                Next
-              </button>
             </div>
           </div>
         </div>
